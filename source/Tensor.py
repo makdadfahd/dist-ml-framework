c7ef8d663228f7aa75adb5158d9082c98cd12d06
Fahd <fahdmakdad48@gmail.com>
2026-07-25 14:13:40 +0100

    initialising tensor class using numpy arrays

--- source/Tensor.py
+++ source/Tensor.py
@@ -1,31 +1,46 @@
 import numpy as np 
 
 class Tensor :
-    def __init__(self, data = None):
-        if data is None :
-            self.data = np.array([])
-        else :
-            self.data = np.array(data)
+    def __init__(self, data = np.array([]), _children = [] ):
+        self.data = data
+        self._prev = set(_children)
+        self.grad = 0.0
+
+    def __repr__(self):
+        return f"Tensor( Data : {self.data} )"
 
     def __add__(self, other):
-        return Tensor(self.data + other.data)
+        out = self.data + other.data
+        return Tensor(out, (self,other))
     
     def __sub__(self,other) :
-        return Tensor(self.data - other.data)
+        out = self.data - other.data
+        return Tensor(out, (self,other))
     
     def __mul__(self, other):
-        return Tensor(self.data * other.data)
+        out = self.data * other.data
+        return Tensor(out , (self,other))
     
     def __truediv__(self, other):
         eps = 1e-8
-        return Tensor(self.data / (other.data + eps))
+        out = self.data / (other.data + eps)
+        return Tensor(out, (self,other))
 
     def __pow__(self, other):
         if isinstance(other, Tensor) :
             exponement = other.data
         else :
             exponement = other
-        return Tensor(self.data**exponement)
+        out = self.data**exponement
+        
+        return Tensor(out,(self,))
 
     def __matmul__(self, other):
-        pass
+        out = self.data @ other.data
+        return Tensor(out,(self,other))
+
+    def __rmatmul__(self, other):
+        return self.data @ other.data
+
+
+
