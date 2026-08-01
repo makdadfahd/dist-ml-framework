48a2195a8abfa0da4a8bd5f19778c38db5f7212d
Fahd <fahdmakdad48@gmail.com>
2026-08-01 19:37:31 +0100

    first version of scalar engine completed

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math as m
 
 class Value : 
     def __init__(self, data, _children=()):
@@ -8,40 +9,87 @@ class Value :
         self._backward = lambda : None 
 
     def __repr__(self):
-        return f'Data : {self.data}'
+        return f'Value(Data : {self.data})'
 
     def __add__(self, other) :
+        other = other if isinstance(other,Value) else Value(other)
         out = Value(self.data + other.data, (self,other))
 
         def _backward() : 
-            self.grad = 1.0 * out.grad 
-            other.grad = 1.0 * out.grad
+            self.grad += 1.0 * out.grad 
+            other.grad += 1.0 * out.grad
         out._backward = _backward
 
         return out
 
     def __mul__(self,other) :
+        other = other if isinstance(other,Value) else Value(other)
         out = Value(self.data * other.data, (self,other)) 
 
         def _backward() : 
-            self.grad = other.data * out.grad
-            other.grad = self.data * out.grad
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
         out._backward = _backward
         
         return out
 
+    def __rmul__(self, other):
+        return self * other 
+
+    def __pow__(self, other):
+        assert isinstance(other,(int,float)) , "only supporting int/float powers for now"
+        out = Value(self.data**other, (self,))
+
+        def _backward(self) :
+            self.grad += other * self.data**(other - 1) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def tanh(self) : 
+        x = self.data 
+        t = (m.exp(x)**2 -  1) / (m.exp(x)**2 + 1)
+        out = Value(t, (self,))
+
+        def _backward() :
+            self.grad = (1 - t**2) * out.grad 
+        out._backward = _backward
+
+        return out
 
 
-a = Value(2.0)
-b = Value(3.0)
-c = Value(9.3)
-d = a + b
-e = d * c 
-f = Value(0.5)
-g = e * f 
+    def backward(self) :
 
-g.grad = 1.0
+        topo = []
+        visited = set()
+        def topo_sort(node) :
+            if node not in visited :
+                visited.add(node)
+                for child in node._prev :
+                    topo_sort(child)
+                topo.append(node)
+        topo_sort(self)
+        self.grad = 1.0
 
-g._backward()
-e._backward()
-print(d.grad)
+        for node in reversed(topo) :
+            node._backward()
+
+    def __neg__(self) :
+        return self * (-1)
+
+    def __sub__(self,other) :
+        return self + (- other )
+
+    def __rsub__(self, other):
+        return self + (-other)
+    
+    def __radd__(self, other):
+        return self + other
+
+    def __truediv__(self, other):
+        return self * (other**(-1))
+
+    def __rtruediv__(self, other):
+        return other * (self**(-1))
+
+
