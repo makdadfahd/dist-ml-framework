ca3b1aa8fc87c734902a85480e75b78dba1ead0c
Fahd <fahdmakdad48@gmail.com>
2026-08-30 23:07:20 +0100

    adding learn and predict methods inside MLP to save time in next benchmarks

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -1,5 +1,21 @@
 from multi_dim_engine import Tensor
+from optimizer import Adam
 import numpy as np
+import torch
+from torchvision import datasets
+
+#loading the training dataset :
+
+train_dataset = datasets.MNIST(root='./data', train=True, download=True)
+X_train = train_dataset.data.numpy().reshape(-1, 784).astype('float32') / 255.0
+y_train = train_dataset.targets.numpy()
+
+#loading the test dataset :
+
+test_dataset = datasets.MNIST(root="./data", train=False, download=True)
+X_test = test_dataset.data.numpy().reshape(-1, 784).astype("float32") / 255.0
+y_test = test_dataset.targets.numpy()
+
 
 def cross_entropy_loss(index_vector,scores) :
     probabilities = softmax(scores)
@@ -69,3 +85,49 @@ class MLP :
         for layer in self.layers :
             params.extend(layer.parameters())
         return params
+
+    def learn(self,batch_size, epochs = 20) :
+        num_samples = X_train.shape[0]
+        optimizer = Adam(self.parameters(), 0.003)  
+
+        for epoch in range(epochs):
+            indices = np.arange(num_samples)
+            np.random.shuffle(indices)
+
+            running_loss = 0.0
+            num_batches = num_samples // batch_size
+
+            for i in range(num_batches):
+                idx = indices[i * batch_size : (i + 1) * batch_size]
+                x_batch = Tensor(X_train[idx])
+                y_batch = y_train[idx]
+
+                scores = self(x_batch)
+                loss = cross_entropy_loss(y_batch, scores)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.data
+            print(
+                    f"Epoch {epoch + 1} | Average Loss = {running_loss / num_batches:.4f}"
+                )
+
+        test_scores = self(Tensor(X_train))
+        test_preds = np.argmax(test_scores.data, axis=1)
+
+        accuracy = np.mean(test_preds == y_train) * 100
+        print(f"Accuracy on training data : {accuracy:.2f}%")
+
+    def pred(self, batch_size) :
+        indices = np.random.choice(len(X_test), size=batch_size, replace=False)
+        x_sample = Tensor(X_test[indices])
+        y_true = y_test[indices]
+
+        scores = self(x_sample)
+        predictions = np.argmax(scores.data, axis=1)
+
+        accuracy = np.mean(predictions == y_true) * 100
+        print(f"Test Accuracy: {accuracy:.2f}%")
+
