f994782568a3c51a2856c3bffe03de6bec67ae16
Fahd <fahdmakdad48@gmail.com>
2026-09-05 21:09:23 +0100

    Managed to send and receive a Tensor data from a server to a client using pickle library

--- src/network/client.py
+++ src/network/client.py
@@ -1,36 +1,23 @@
 import socket 
-import threading
-
+import numpy as np
+import pickle
+from micrograd.multi_dim_engine import Tensor
 
 #implement the constants :
 dest_address = socket.gethostbyname(socket.gethostname())
 dest_port = 5050
-encoder = 'utf-8'
-name = input('your name please : ')
-bytes_number = 1024
+
 
 #intialize the client socket, and connect to the server :
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((dest_address, dest_port))
 
-#def some functions :
-def receive_message():
-    while True :
-        global bytes_number , client_socket , encoder
-        message = client_socket.recv(bytes_number).decode(encoder)
-        if message == 'allias' :
-            client_socket.send(name.encode(encoder))
-        else :
-            print(message)
 
-def send_message() :
-    while True :
-        global client_socket , encoder 
-        message = input()
-        client_socket.send(message.encode(encoder))
+#receiving the array data
+data = client_socket.recv(4096)
+tensor = Tensor(pickle.loads(data))
+print(f"{tensor}")
 
-#now we move to threading :
-t1 = threading.Thread(target=receive_message)
-t2 = threading.Thread(target=send_message)
-t1.start()
-t2.start()
+
+#closing the client socket after receiving the array
+client_socket.close()
