d67665d96574c8dbf21f3ed2d8fdcf4e4aa3a03d
Fahd <fahdmakdad48@gmail.com>
2026-09-15 21:50:36 +0100

    started coding a serve_request method that receives the function and args and dispatch them

--- src/network/rpcserver.py
+++ src/network/rpcserver.py
@@ -32,3 +32,22 @@ class RPCServer() :
                 'message' : 'function is not defined'
             }
         return response
+
+    def serve_request(self) :
+        #we receive the json info length 
+        info_length = self.conn._recv_exact(4)
+        len_json_info = struct.unpack('!I', info_length)[0]
+
+        #we call to receive the exact bytes number received in the length of the json info
+        json_info = self.conn._recv_exact(len_json_info).decode('utf-8')
+
+        #we load the request of the client, we get the function name and args (if they exist), and we dispatch them to give a response
+        request = json.loads(json_info)
+        func = request["function_name"]
+        args = request["args"]
+        response = self.dispatch(func , args)
+
+        #we send the response back to the client
+
+    def call(self, function_name, arguments) :
+        pass
