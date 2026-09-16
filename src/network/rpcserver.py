ceecc648c4f7073c44095ea9a6b558963cd0cccc
Fahd <fahdmakdad48@gmail.com>
2026-09-16 22:17:54 +0100

    completed the serve_request and call methods, rpc server part is fully finished :)

--- src/network/rpcserver.py
+++ src/network/rpcserver.py
@@ -1,10 +1,13 @@
 import socket , struct , json
 from network.tensorconnection import TensorConnection
 
+def add(*args) :
+    return sum(args)
+
 class RPCServer() :   
-    def __init__(self) :
+    def __init__(self, socket) :
         self.conn = TensorConnection(socket)
-        self.functions = {}
+        self.functions = {"add" : add}
 
     def dispatch(self, function_name , args = []) :
         try :
@@ -39,15 +42,39 @@ class RPCServer() :
         len_json_info = struct.unpack('!I', info_length)[0]
 
         #we call to receive the exact bytes number received in the length of the json info
-        json_info = self.conn._recv_exact(len_json_info).decode('utf-8')
+        request_json = self.conn._recv_exact(len_json_info).decode('utf-8')
 
         #we load the request of the client, we get the function name and args (if they exist), and we dispatch them to give a response
-        request = json.loads(json_info)
+        request = json.loads(request_json)
         func = request["function_name"]
         args = request["args"]
         response = self.dispatch(func , args)
 
-        #we send the response back to the client
+        #we send the response back 
+        response_json = json.dumps(response).encode('utf-8')
+        self.conn._send_msg(response_json)
+
 
     def call(self, function_name, arguments) :
-        pass
+        #we send the request
+        request = {
+            "function_name" : function_name,
+            "args" : arguments
+        }
+        request_json = json.dumps(request).encode('utf-8')
+        self.conn._send_msg(request_json)
+
+        #we receive the response 
+        response_length = self.conn._recv_exact(4)
+        len_response_json = struct.unpack('!I', response_length)[0]
+
+        response_json = self.conn._recv_exact(len_response_json).decode('utf-8')
+        response = json.loads(response_json)
+
+        status = response["status"]
+
+        #we check the response status , if true we get the response if not we get the alert message as an error
+        if status : 
+            return response["result"]
+        else :
+            raise RuntimeError(response["message"])
