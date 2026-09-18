d0b28bf5f545ab26b129dc53bc55cc6d4ec4e0bd
Fahd <fahdmakdad48@gmail.com>
2026-09-18 14:20:46 +0100

    fixing a bug in ._send_msg, methods takes python dictionnary instead of json_data as bytes

--- src/network/tensorconnection.py
+++ src/network/tensorconnection.py
@@ -7,36 +7,27 @@ class TensorConnection :
         self.socket = socket
 
     def send_tensor(self, tensor) :
-        #getting the tensor info and data bytes :
         tensor_info , tensor_data_bytes = self.tensor_info(tensor)
-        json_info = json.dumps(tensor_info).encode('utf-8')
-
-        #sending them over the network
-        self.socket._send_msg(json_info)
+        self._send_msg(tensor_info)
         self.socket.sendall(tensor_data_bytes)
 
     def recv_tensor(self) :
-        json_info = self.recv_json()
-        tensor_info = json.loads(json_info)
+        tensor_info = self._recv_msg()
         tensor = self.construct_tensor(tensor_info)
         return tensor
-
 
     def _recv_exact(self, n):
         full_msg = b''
         while len(full_msg) < n :
             chunk = self.socket.recv(n - len(full_msg))
-            # a small safety check so we don't receive empty bytes in case of a connection problem
-            if not chunk :
+            if not chunk : #safety check
                 raise ConnectionError("Connection problem while receiving data !!")
             full_msg += chunk
         return full_msg
 
-    def _send_msg(self, json_data:bytes) :
-        #we get the json data length
+    def _send_msg(self, msg) :
+        json_data = json.dumps(msg).encode('utf-8')
         len_data = struct.pack('!I', len(json_data))
-
-        #we send the data length and data as bytes
         self.socket.sendall(len_data)
         self.socket.sendall(json_data)
 
@@ -57,8 +48,9 @@ class TensorConnection :
         tensor = Tensor(tensor_data.reshape(tensor_dict["shape"]))
         return tensor
 
-    def recv_json(self) :
+    def _recv_msg(self) :
         info_length = self._recv_exact(4)
         length_json_info = struct.unpack('!I', info_length)[0]
         json_info = self._recv_exact(length_json_info).decode('utf-8')
-        return json_info
+        msg = json.loads(json_info)
+        return msg
