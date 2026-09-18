61b56d7586e120d2e4abacd9872d8601b7edd414
Fahd <fahdmakdad48@gmail.com>
2026-09-18 10:31:26 +0100

    used threading library, now the server can handle multiple clients and multiple requests at the same time, already verified in the script

--- src/network/client.py
+++ src/network/client.py
@@ -8,7 +8,8 @@ from network.rpcserver import RPCServer
 #implement the constants :
 dest_address = socket.gethostbyname(socket.gethostname())
 dest_port = 5050
-
+A = Tensor([[1,2,3], [4,5,6], [7,8,9]])
+B = Tensor([[1,0,0], [0,1,0], [0,0,1]])
 
 #intialize the client socket, and connect to the server :
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,12 +18,11 @@ client_socket.connect((dest_address, dest_port))
 
 #receiving the Tensor from master
 worker = RPCServer(client_socket)
+while True :
+    print("We do you want to ask the server")
+    args = [int(x) for x in input("Enter numbers: ").split()]
+    response = worker.call("add", args)
+    print(f'The answer is : {response}')
 
-print("We will ask the server what is 2 + 3 ?")
-print("Waiting for response...")
-response = worker.call("add", [2,3,4,5,6])
-print(f'The answer is : {response}')
-
-print("\nResponse received successfully...")
-#closing the client socket after receiving the array
-client_socket.close()
+    print("\nResponse received successfully...")
+    
--- src/network/server.py
+++ src/network/server.py
@@ -3,16 +3,11 @@ import numpy as np
 from micrograd.multi_dim_engine import Tensor
 from network.tensorconnection import TensorConnection
 from network.rpcserver import RPCServer
-import time
-
+import threading
 
 #implement the constants :
 server_address = socket.gethostbyname(socket.gethostname())
 port = 5050
-test_tensor = Tensor([[1 , 2 , 3],
-                 [4 , 5 , 6]     ,
-                 [7 , 8 , 9]])
-
 
 #initialise the server socket, bind it to (ADDRESS, PORT) and make the server waiting for connection :
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,18 +16,10 @@ server_socket.listen()
 
 
 #receiving the clients connection
-clientsocket , address = server_socket.accept()
-print(f"Connection received from {address}\n")
-
-
-#sending tensor to the worker
-
-master = RPCServer(clientsocket)
-
-time.sleep(10)
-master.serve_request()
-print("Response sent successfully !!")
-
-#closing client and server socket
-clientsocket.close()
-server_socket.close()
+while True :
+    workersocket , address = server_socket.accept()
+    print(f"Connection received from {address}\n")
+    master = RPCServer(workersocket)
+    #opening threads so we can handle multiple clients
+    t = threading.Thread(target= master.handle_client)
+    t.start()
