f237c6f10fab9c10cf0705ec64a21686e6533d3e
Fahd <fahdmakdad48@gmail.com>
2026-09-20 14:05:10 +0000

    started organizing the environement for the first test of a master (server) and worker (client) rpc training on MNIST dataset

--- src/network/client.py
+++ src/network/client.py
@@ -2,7 +2,7 @@ import socket
 import numpy as np
 from micrograd.multi_dim_engine import Tensor
 from network.tensorconnection import TensorConnection
-from network.rpcserver import RPCServer
+from network.rpcserver import RPCServer , RPCClient
 
 
 #implement the constants :
@@ -17,7 +17,7 @@ client_socket.connect((dest_address, dest_port))
 
 
 #receiving the Tensor from master
-worker = RPCServer(client_socket)
+worker = RPCClient(client_socket)
 
 response = worker.call("add", [2,B])
 print(f'The answer is : {response}')
--- src/network/server.py
+++ src/network/server.py
@@ -1,9 +1,23 @@
-import socket 
-import numpy as np
+import socket , threading
 from micrograd.multi_dim_engine import Tensor
 from network.tensorconnection import TensorConnection
 from network.rpcserver import RPCServer
-import threading
+from micrograd.optimizer import Adam
+from torchvision import datasets
+import numpy as np
+from pytorch_comparaison.mymodel import Engine_Model
+
+#loading the datasets
+train_dataset = datasets.MNIST(root='./data', train=True, download=True)
+test_dataset = datasets.MNIST(root='./data', train=False, download=True)
+
+
+#organizing it in numpy arrays
+x_train = train_dataset.data.numpy().reshape(-1, 784).astype('float32') / 255.0
+y_train = train_dataset.targets.numpy()
+
+x_test = test_dataset.data.numpy().reshape(-1, 784).astype('float32') / 255.0
+y_test = test_dataset.targets.numpy()
 
 #implement the constants :
 server_address = socket.gethostbyname(socket.gethostname())
@@ -15,11 +29,17 @@ server_socket.bind((server_address, port))
 server_socket.listen()
 
 
+#get the parameters and initialise the optimizer
+model = Engine_Model()
+parameters = model.parameters()
+optimizer = Adam(parameters, lr = 0.003)
+
 #receiving the clients connection
 while True :
     workersocket , address = server_socket.accept()
     print(f"Connection received from {address}\n")
-    master = RPCServer(workersocket)
+    master = RPCServer(workersocket, parameters, optimizer)
+
     #opening threads so we can handle multiple clients
     t = threading.Thread(target= master.handle_client)
     t.start()
