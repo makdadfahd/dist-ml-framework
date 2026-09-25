a68788b36271441e59abd108069c57cb7e506b2a
Fahd <fahdmakdad48@gmail.com>
2026-09-25 16:05:08 +0000

    tested the new method (get_weights) , the worker got the weights from the server successfully

--- src/network/client.py
+++ src/network/client.py
@@ -1,27 +1,15 @@
-import socket 
-import numpy as np
-from micrograd.multi_dim_engine import Tensor
-from network.tensorconnection import TensorConnection
-from network.rpcserver import RPCServer , RPCClient
+import socket
+from network.rpcserver import RPCClient
 
 
-#implement the constants :
-dest_address = socket.gethostbyname(socket.gethostname())
-dest_port = 5050
-A = Tensor([[1,2,3], [4,5,6], [7,8,9]])
-B = Tensor([[1,0,0], [0,1,0], [0,0,1]])
+address = socket.gethostbyname(socket.gethostname())
 
-#intialize the client socket, and connect to the server :
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((dest_address, dest_port))
+#initialise the client socket and connect to the server
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket.connect((address, 5050))
 
+worker = RPCClient(clientsocket)
 
-#receiving the Tensor from master
-worker = RPCClient(client_socket)
+weights = worker.call("get_weights")
 
-response = worker.call("add", [2,B])
-print(f'The answer is : {response}')
-
-print("\nResponse received successfully...")
-    
-client_socket.close()
+print(weights)
--- src/network/server.py
+++ src/network/server.py
@@ -1,45 +1,27 @@
-import socket , threading
-from micrograd.multi_dim_engine import Tensor
-from network.tensorconnection import TensorConnection
+import socket 
 from network.rpcserver import RPCServer
+from micrograd.neural_network import MLP
 from micrograd.optimizer import Adam
-from torchvision import datasets
-import numpy as np
-from pytorch_comparaison.mymodel import Engine_Model
 
-#loading the datasets
-train_dataset = datasets.MNIST(root='./data', train=True, download=True)
-test_dataset = datasets.MNIST(root='./data', train=False, download=True)
+address = socket.gethostbyname(socket.gethostname())
 
+#intialise the server socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind((address, 5050))
+serversocket.listen()
 
-#organizing it in numpy arrays
-x_train = train_dataset.data.numpy().reshape(-1, 784).astype('float32') / 255.0
-y_train = train_dataset.targets.numpy()
+#receive the client connection
+clientsocket , addr = serversocket.accept()
 
-x_test = test_dataset.data.numpy().reshape(-1, 784).astype('float32') / 255.0
-y_test = test_dataset.targets.numpy()
+model = MLP(3, [2,2,1])
+params = [] 
 
-#implement the constants :
-server_address = socket.gethostbyname(socket.gethostname())
-port = 5050
+for param in model.parameters() :
+    params.append(param)
 
-#initialise the server socket, bind it to (ADDRESS, PORT) and make the server waiting for connection :
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((server_address, port))
-server_socket.listen()
+optimizer = Adam(params)
 
+master = RPCServer(clientsocket, params, optimizer)
 
-#get the parameters and initialise the optimizer
-model = Engine_Model()
-parameters = model.parameters()
-optimizer = Adam(parameters, lr = 0.003)
-
-#receiving the clients connection
 while True :
-    workersocket , address = server_socket.accept()
-    print(f"Connection received from {address}\n")
-    master = RPCServer(workersocket, parameters, optimizer)
-
-    #opening threads so we can handle multiple clients
-    t = threading.Thread(target= master.handle_client)
-    t.start()
+    master.handle_client()
